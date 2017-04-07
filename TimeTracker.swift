706b15ad251e8f8b758ee543c0226bfda388610e
Tiago Martinho <tiago@elit.software>
2017-04-07 12:15:07 +0200

    Extract TimeTracker to separate file

---
+++ TimeTracker.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+class TimeTracker {
+
+    var startDate: Date?
+    var endDate: Date?
+
+    private let dateProvider: DateProvider
+
+    var currentTime: TimeInterval? {
+        guard let startDate = startDate else { return nil }
+        return dateProvider.currentDate.timeIntervalSince(startDate)
+    }
+
+    init(dateProvider: DateProvider) {
+        self.dateProvider = dateProvider
+    }
+
+    func start() {
+        startDate = dateProvider.currentDate
+    }
+
+    func stop() {
+        endDate = dateProvider.currentDate
+    }
+}
