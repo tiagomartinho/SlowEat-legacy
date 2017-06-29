e53e622f650e43f7fb6fb9f26ffc0e59c6db75bb
Tiago Martinho <tiago@elit.software>
2017-06-29 22:23:27 +0200

    Define MealsViewController and set it as the root vc

--- SlowEat/AppDelegate.swift
+++ SlowEat/AppDelegate.swift
@@ -1,54 +1,33 @@
 import UIKit
 import Instabug
 import Mixpanel
-import WatchConnectivity
-import AudioToolbox
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var session: WCSession? {
-        didSet {
-            if let session = session {
-                session.delegate = self
-                session.activate()
-            }
-        }
-    }
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window?.tintColor = #colorLiteral(red: 0, green: 0.8784313725, blue: 0.5176470588, alpha: 1)
-
-        Instabug.start(withToken: "7cdc3b33e85559115d49a0941cd6a89d", invocationEvent: .shake)
-
-        let token = "7afd4b44a2d4522c389212c59ce66886"
-        let mixpanel = Mixpanel.sharedInstance(withToken: token)
-        mixpanel.enableLogging = false
-
-        session = WCSession.default()
-
+        setupSDK()
+        setTintColor()
+        setRootViewController()
         return true
     }
 
-    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
-    func sessionDidBecomeInactive(_ session: WCSession) {}
-    func sessionDidDeactivate(_ session: WCSession) {}
+    private func setupSDK() {
+        Instabug.start(withToken: "7cdc3b33e85559115d49a0941cd6a89d", invocationEvent: .shake)
+        let token = "7afd4b44a2d4522c389212c59ce66886"
+        let mixpanel = Mixpanel.sharedInstance(withToken: token)
+        mixpanel.enableLogging = false
+    }
 
-    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-        if let _ = message["vibrate"] {
-//            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-        }
-        if let _ = message["start"] {
-            let mixpanel = Mixpanel.sharedInstance()
-            mixpanel.timeEvent("meal")
-            print("start meal")
-        }
-        if let _ = message["end"] {
-            let mixpanel = Mixpanel.sharedInstance()
-            mixpanel.track("meal")
-            print("end meal")
-        }
+    private func setTintColor() {
+        window?.tintColor = #colorLiteral(red: 0, green: 0.8784313725, blue: 0.5176470588, alpha: 1)
+    }
+
+    private func setRootViewController() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MealsViewController()
+        window?.makeKeyAndVisible()
     }
 }
---
+++ SlowEat/MealsViewController.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class MealsViewController: UIViewController {
+
+}
