d9c61b29d91d758f43d6a53b1b416da5651ce68e
Tiago Martinho <tiago@elit.software>
2018-01-03 08:24:19 +0100

    Refactor MealAnalyser

--- MealAnalyser.swift
+++ MealAnalyser.swift
@@ -1,15 +1,10 @@
 class MealAnalyser {
     func analyse(meal: Meal) -> Meal {
-        var isMoving = false
         var movingCount = 0
         let processedEvents = meal.events.map { event -> Event in
             let processedEvent = movingCount > 0 && movingCount < 10 ? Event(type: .waiting, date: event.date) : event
-            isMoving = (event.type == .moving)
-            if isMoving {
-                movingCount += 1
-                if movingCount > 10 {
-                    movingCount = 1
-                }
+            if event.type == .moving {
+                movingCount = (movingCount + 1) % 10
             } else {
                 movingCount = 0
             }
