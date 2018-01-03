7f39cd13733122a76b35e57947fce237c1692e40
Tiago Martinho <tiago@elit.software>
2018-01-03 08:26:33 +0100

    Extract isMoving, use ternary instead of if

--- MealAnalyser.swift
+++ MealAnalyser.swift
@@ -4,11 +4,8 @@ class MealAnalyser {
         let processedEvents = meal.events.map { event -> Event in
             let shouldGroupMovingEvent = movingCount > 0 && movingCount < 10
             let processedEvent = shouldGroupMovingEvent ? Event(type: .waiting, date: event.date) : event
-            if event.type == .moving {
-                movingCount = (movingCount + 1) % 10
-            } else {
-                movingCount = 0
-            }
+            let isMoving = (event.type == .moving)
+            movingCount = isMoving ? (movingCount + 1) % 10 : 0
             return processedEvent
         }
         return Meal(events: processedEvents)
