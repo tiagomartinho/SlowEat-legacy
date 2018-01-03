8d67d9410e65c3086e3229523b0f71b330d695be
Tiago Martinho <tiago@elit.software>
2018-01-03 08:25:30 +0100

    Extract shouldGroupMovingEvent var

--- MealAnalyser.swift
+++ MealAnalyser.swift
@@ -2,7 +2,8 @@ class MealAnalyser {
     func analyse(meal: Meal) -> Meal {
         var movingCount = 0
         let processedEvents = meal.events.map { event -> Event in
-            let processedEvent = movingCount > 0 && movingCount < 10 ? Event(type: .waiting, date: event.date) : event
+            let shouldGroupMovingEvent = movingCount > 0 && movingCount < 10
+            let processedEvent = shouldGroupMovingEvent ? Event(type: .waiting, date: event.date) : event
             if event.type == .moving {
                 movingCount = (movingCount + 1) % 10
             } else {
