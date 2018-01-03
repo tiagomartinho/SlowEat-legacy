b51bbdb055b9af6fca1e314ee45fc8db3e292548
Tiago Martinho <tiago@elit.software>
2018-01-03 08:21:33 +0100

    Group moving events

--- MealAnalyser.swift
+++ MealAnalyser.swift
@@ -1,9 +1,18 @@
 class MealAnalyser {
     func analyse(meal: Meal) -> Meal {
         var isMoving = false
+        var movingCount = 0
         let processedEvents = meal.events.map { event -> Event in
-            let processedEvent = isMoving ? Event(type: .waiting, date: event.date) : event
+            let processedEvent = movingCount > 0 && movingCount < 10 ? Event(type: .waiting, date: event.date) : event
             isMoving = (event.type == .moving)
+            if isMoving {
+                movingCount += 1
+                if movingCount > 10 {
+                    movingCount = 1
+                }
+            } else {
+                movingCount = 0
+            }
             return processedEvent
         }
         return Meal(events: processedEvents)
--- SlowEatTests/MealAnalyserTest.swift
+++ SlowEatTests/MealAnalyserTest.swift
@@ -23,7 +23,7 @@ class MealAnalyserTest: XCTestCase {
 
         XCTAssertEqual(nonStopMeal.events.count, mealAnalysed.events.count)
         XCTAssertEqual(3, mealAnalysed.events.filter { $0.type == .moving }.count)
-        XCTAssertEqual(.moving, mealAnalysed.events[11].type)
+        XCTAssertEqual(.moving, mealAnalysed.events[10].type)
         XCTAssertEqual(.waiting, mealAnalysed.events[12].type)
     }
 
