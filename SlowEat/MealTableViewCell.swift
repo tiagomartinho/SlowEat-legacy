3b80cd397054cf96f2eb6b21839018f0ec11e0a3
Tiago Martinho <tiago@elit.software>
2018-01-14 12:45:45 +0100

    Refactor MealTableViewCell

--- SlowEat/MealTableViewCell.swift
+++ SlowEat/MealTableViewCell.swift
@@ -11,40 +11,6 @@ class MealTableViewCell: TableViewCell {
 
     private let defaultMargin = 10.0
 
-    override func initView() {
-        setViewBackground()
-        initDateLabel()
-        initBpmLabel()
-        initPercentageLabel()
-        addDateLabel()
-        addBpmLabel()
-        addPercentageLabel()
-    }
-
-    private func setViewBackground() {
-        backgroundColor = .black
-    }
-
-    private func initDateLabel() {
-        dateLabel = UILabel()
-        dateLabel.textColor = UIColor.white.withAlphaComponent(0.25)
-    }
-
-    private func addDateLabel() {
-        addSubview(dateLabel)
-        dateLabel.snp.makeConstraints { maker in
-            maker.leading.equalToSuperview().inset(defaultMargin)
-            maker.top.equalToSuperview().inset(defaultMargin)
-        }
-    }
-
-    private func initBpmLabel() {
-        bpmLabel = UILabel()
-        bpmLabel.adjustsFontSizeToFitWidth = true
-        bpmLabel.textAlignment = .right
-        bpmView = UIView()
-    }
-
     func set(bpm: String) {
         let size = UIFont.preferredFont(forTextStyle: .title3).pointSize
         let attributesValue = [
@@ -74,7 +40,53 @@ class MealTableViewCell: TableViewCell {
         percentageView.backgroundColor = color
     }
 
-    private func addBpmLabel() {
+    override func initView() {
+        setViewBackground()
+        initDateLabel()
+        initBpmView()
+        initPercentageView()
+        addDateLabel()
+        addBpmView()
+        addPercentageView()
+    }
+
+    private func setViewBackground() {
+        backgroundColor = .black
+    }
+
+    private func initDateLabel() {
+        dateLabel = UILabel()
+        dateLabel.textColor = UIColor.white.withAlphaComponent(0.25)
+    }
+
+    private func initBpmView() {
+        bpmLabel = UILabel()
+        bpmLabel.adjustsFontSizeToFitWidth = true
+        bpmLabel.textAlignment = .right
+        bpmView = UIView()
+    }
+
+    private func initPercentageView() {
+        percentageLabel = UILabel()
+        percentageLabel.adjustsFontSizeToFitWidth = true
+        percentageLabel.textColor = .white
+        percentageLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        percentageLabel.textAlignment = .right
+
+        percentageView = UIView()
+        percentageView.layer.cornerRadius = 8.0
+        percentageView.layer.masksToBounds = true
+    }
+
+    private func addDateLabel() {
+        addSubview(dateLabel)
+        dateLabel.snp.makeConstraints { maker in
+            maker.leading.equalToSuperview().inset(defaultMargin)
+            maker.top.equalToSuperview().inset(defaultMargin)
+        }
+    }
+
+    private func addBpmView() {
         bpmView.addSubview(bpmLabel)
         addSubview(bpmView)
         bpmLabel.snp.makeConstraints { maker in
@@ -87,24 +99,12 @@ class MealTableViewCell: TableViewCell {
             maker.leading.equalToSuperview().inset(defaultMargin)
             maker.bottom.equalToSuperview().inset(defaultMargin)
             maker.top.equalTo(dateLabel.snp.bottom).offset(defaultMargin)
-            maker.width.equalToSuperview().dividedBy(3).priorityMedium()
-            maker.width.lessThanOrEqualTo(200).priorityHigh()
+            maker.width.equalToSuperview().dividedBy(3).priority(.medium)
+            maker.width.lessThanOrEqualTo(200).priority(.high)
         }
     }
 
-    private func initPercentageLabel() {
-        percentageLabel = UILabel()
-        percentageLabel.adjustsFontSizeToFitWidth = true
-        percentageLabel.textColor = .white
-        percentageLabel.font = UIFont.preferredFont(forTextStyle: .title3)
-        percentageLabel.textAlignment = .right
-
-        percentageView = UIView()
-        percentageView.layer.cornerRadius = 8.0
-        percentageView.layer.masksToBounds = true
-    }
-
-    private func addPercentageLabel() {
+    private func addPercentageView() {
         percentageView.addSubview(percentageLabel)
         addSubview(percentageView)
         percentageLabel.snp.makeConstraints { maker in
@@ -117,8 +117,8 @@ class MealTableViewCell: TableViewCell {
             maker.leading.equalTo(bpmView.snp.trailing).offset(defaultMargin)
             maker.bottom.equalToSuperview().inset(defaultMargin)
             maker.top.equalTo(dateLabel.snp.bottom).offset(defaultMargin)
-            maker.width.equalToSuperview().dividedBy(2).priorityMedium()
-            maker.width.lessThanOrEqualTo(200).priorityHigh()
+            maker.width.equalToSuperview().dividedBy(2).priority(.medium)
+            maker.width.lessThanOrEqualTo(200).priority(.high)
         }
     }
 }
